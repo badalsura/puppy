40afe3121d4985ede380f5139f8017d74435e901
badal <satyamsura99@gmail.com>
2024-03-27 11:31:28 +0530

    third

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/badalsura/puppy
 
 go 1.22.0
+
+require github.com/badalsura/dog v0.0.0-20240327055455-f6411108e853
--- puppy.go
+++ puppy.go
@@ -1,9 +1,18 @@
 package puppy
 
+import "github.com/badalsura/dog"
 func Bark() string {
 	return "woof!"
 }
 
 func Barks() string {
 	return "Woof! woof! woorf!"
+}
+
+func BigBark() string {
+	return dog.WhenGrownUp(Bark())
+}
+
+func BigBarks() string {
+	return dog.WhenGrownUp(Barks())
 }
